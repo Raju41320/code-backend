ccc8f4bf3462ee369a691f3d2bf3f9b658325e38
Raju Borkute <rajuborkute2000@gmail.com>
2024-02-28 00:13:06 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,10 @@
 const ascyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
 
-export { ascyncHandler };
+export default ascyncHandler;
 
 /*
 const  ascyncHandler = () => {}
